d542300639a22ca2503a09f2a92dbf105d12f6f4
Harsharan Deep <harsh398@live.com>
2021-05-05 07:29:49 +0200

    Change

--- test/checkpokemon.js
+++ test/checkpokemon.js
@@ -13,8 +13,8 @@ describe('Check Pokemon Api Suite', function () {
     describe('GET Request', function () {
         it('Should Get Pikachu', async function () {
             let res = await apiHelper.sendGETRequest(baseUrl, apiEndPoint);
-            res = res.body;
             res.status.should.equal(200);
+            res = res.body;
             res.should.have.property('name').which.is.a.String('thunder-punch')
             for(let i =0 ; i< 81 ;i++){
                let response_body= res.moves[i].move;
